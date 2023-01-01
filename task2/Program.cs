4c990bf02b161852a44012c611f9834eb116e5a7
Fyodor <feodorinin@gmail.com>
2023-01-01 18:22:15 +0300

    add 4 programms for tasks

---
+++ task2/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+
+Console.Clear();
+
+Console.WriteLine("Введите 3 числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b && a > c)
+{
+    Console.WriteLine($"max -> {a}");
+}
+if (b > a && b > c)
+{
+    Console.WriteLine($"max -> {b}");
+}
+if (c > a && c > b)
+{
+    Console.WriteLine($"max -> {c}");
+}
---
+++ task4/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.Clear();
+
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N % 2 == 0)
+{
+    for (int i = 1; i <= N-2; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write($"{i}, ");
+        }
+    }
+    Console.Write($" {N}");
+}
+else 
+{
+    for(int j=1;j<=N-2;j++)
+    {
+        if(j%2==0)
+        {
+            Console.Write($"{j}, ");
+        }
+    }
+    Console.Write($"{N-1}");
+}
